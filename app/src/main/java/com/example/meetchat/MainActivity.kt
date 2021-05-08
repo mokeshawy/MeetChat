06d8c8a531525d8a181de10bef17282f07d52a55
mohamed <mohamed_gsm@msn.com>
2021-05-08 12:43:44 +0200

    upload first file

---
+++ app/src/main/java/com/example/meetchat/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.meetchat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.meetchat.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this , R.layout.activity_main)
+
+        // operation work for navigation component for fragment
+        val navHostFragment : NavHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController : NavController = navHostFragment.navController
+
+        // set up action bar for fragment
+        val appBarConfiguration = AppBarConfiguration(setOf())
+        setupActionBarWithNavController(navController , appBarConfiguration)
+
+
+
+    }
+}
